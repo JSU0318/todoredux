4b9e9f4668b101af591a5fa2b0fa7aa79513b936
JSU0318 <dnr0000@gmail.com>
2022-12-03 23:37:07 +0900

    첫커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React from "react";
+import styled from "styled-components";
+
+// 1. styled-components를 만들었습니다.
+const StBox = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid ${(props) => props.borderColor}; // 4.부모 컴포넌트에서 보낸 props를 받아 사용합니다.
+  margin: 20px;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 2. 그리고 위에서 만든 styled-components를 사용했습니다. */}
+      {/* 3. 그리고 props를 통해 borderColor라는 값을 전달했습니다. */}
+      <StBox borderColor="red">빨간 박스</StBox>
+      <StBox borderColor="green">초록 박스</StBox>
+      <StBox borderColor="blue">파랑 박스</StBox>
     </div>
   );
-}
+};
 
 export default App;
