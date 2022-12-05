a3e35b9367422a4b50dc2dd5a54332f72d316d83
JSU0318 <dnr0000@gmail.com>
2022-12-05 22:38:13 +0900

    2차

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import React from "react";
 import { useSelector } from "react-redux"; // import 해주세요.
 
 const App = () => {
+  const counterStore = useSelector((state) => state);
+  const number = useSelector((state) => state.counter.number);
+  console.log(counterStore);
+  console.log(number);
   return <div></div>;
 };
 
