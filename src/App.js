030cac234e34e335ea8afb69d62e7de67c7d9d14
JSU0318 <dnr0000@gmail.com>
2022-12-05 19:39:13 +0900

    초기

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 // src/App.js
 
 import React from "react";
-import styled from "styled-components";
-
-// 1. styled-components를 만들었습니다.
-const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid ${(props) => props.borderColor};
-  // 4.부모 컴포넌트에서 보낸 props를 받아 사용합니다.
-  margin: 20px;
-`;
+import { useSelector } from "react-redux"; // import 해주세요.
 
 const App = () => {
-  return (
-    <div>
-      {/* 2. 그리고 위에서 만든 styled-components를 사용했습니다. */}
-      {/* 3. 그리고 props를 통해 borderColor라는 값을 전달했습니다. */}
-      <StBox borderColor="red">빨간 박스</StBox>
-      <StBox borderColor="green">초록 박스</StBox>
-      <StBox borderColor="blue">파랑 박스</StBox>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
