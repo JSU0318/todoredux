679025c234f4babda369b1b74b9fbc27798e0649
JSU0318 <dnr0000@gmail.com>
2022-12-06 19:14:57 +0900

    최종

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 // src/App.js
 
 import React from "react";
-import { useSelector } from "react-redux"; // import 해주세요.
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addNumber, minusNumber } from "./redux/modules/counter";
 
 const App = () => {
-  const counterStore = useSelector((state) => state);
-  const number = useSelector((state) => state.counter.number);
-  console.log(counterStore);
+  const [number, setNumber] = useState(0);
+  const globalNumber = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onChangeHandler = (event) => {
+    const { value } = event.target;
+    setNumber(+value);
+  };
+  const onClickAddNumberHandler = () => {
+    dispatch(addNumber(number));
+  };
+  const onClickMinusNumberHandler = () => {
+    dispatch(minusNumber(number));
+  };
   console.log(number);
-  return <div></div>;
+  return (
+    <div>
+      {globalNumber}
+      <input type="number" onChange={onChangeHandler} />
+      <button onClick={onClickAddNumberHandler}>더하기</button>
+      <button onClick={onClickMinusNumberHandler}>빼기</button>
+    </div>
+  );
 };
 
 export default App;
